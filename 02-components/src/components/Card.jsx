6dc42ffa3f88d4ae7bc9c34f093701c28c45da05
Naresh kumar <nareshkumarcs28@gmail.com>
2025-12-24 01:32:19 +0530

    02-components

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = () => {
+const name ="naresh kumar";
+const age =45;
+
+  return (
+    <div  className="card">
+      <h1>{name}</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem, voluptates quasi cum velit ratione cumque, illum, aliquam nam vero sint odit ipsam suscipit. Non necessitatibus, accusantium recusandae nesciunt placeat asperiores?</p>
+    </div>
+  )
+}
+
+export default Card
